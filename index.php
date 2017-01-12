750581cfbe91b5e5b99f1ad114151456de071c2d
Joseph Ramirez <jramirez98@cnm.edu>
2017-01-11 21:33:06 -0700

    Index page set up. added persona information.

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Strava Trail-by-Trail DDP</title>
+	</head>
+	<!-- Persona, Use Case, Interaction Flow, and Conceptual Model-->
+	<body>
+		<header>
+			<h1>Strava Trail-by-Trail DDP</h1>
+		</header>
+		<main>
+			<!-- Name, Age, Occupation,Technology, Needs, Goals-->
+			<h2>Persona</h2>
+			<ul><b>Name:</b> Morrissey, Steve</ul>
+			<ul><b>Age:</b> 27 years-old</ul>
+				<ul><b>Occupation:</b> </ul>
+				<ul><b>Technology:</b> </ul>
+				<ul><b>Needs:</b> </ul>
+				<ul><b>Goals:</b> </ul>
+
+			<h2>Use Case</h2>
+				<p></p>
+			<h2>Interaction Flow</h2>
+				<p></p>
+			<h2>Conceptual Model</h2>
+				<p></p>
+		</main>
+	</body>
+</html>
+
