86d7a316c4d1be313c712a5e9f1efabc577258d1
Joseph Ramirez <jramirez98@cnm.edu>
2017-01-12 09:06:18 -0700

    added strong tags and description for use case and interaction flow.

--- index.php
+++ index.php
@@ -12,19 +12,27 @@
 		<main>
 			<!-- Name, Age, Occupation,Technology, Needs, Goals-->
 			<h2>Persona</h2>
-			<ul><b>Name:</b> Morrissey, Steve</ul>
-			<ul><b>Age:</b> 27 years-old</ul>
-				<ul><b>Occupation:</b> Works in Bloomingdale's men's fashionable department in Palo Alto, Calif.</ul>
-				<ul><b>Technology:</b> Uses Iphone 7.</ul>
-				<ul><b>Needs:</b> Steve biggest hobby is competing in single-day and time-trial bicycle races. So he needs to keep track of distance and routes he rides. And, he is also appearance and health conscious, so working in in Palo Alto, he is able to to ride his bicycle to and from work.</ul>
-				<ul><b>Goals:</b> He lives in a large cyclists community, so he wants to see other routes where other cyclists have rode on or discovered.</ul>
+			<ul><strong>Name:</strong> Morrissey, Steve</ul>
+			<ul><strong>Age:</strong> 27 years-old</ul>
+				<ul><strong>Occupation:</strong> Works in Bloomingdale's men's fashionable department in Palo Alto, Calif.</ul>
+				<ul><strong>Technology:</strong> Uses Iphone 7.</ul>
+				<ul><strong>Needs:</strong> Steve biggest hobby is competing in single-day and time-trial bicycle races. So he needs to keep track of distance and routes he rides. And, he is also appearance and health conscious, so working in in Palo Alto, he is able to to ride his bicycle to and from work.</ul>
+				<ul><strong>Goals:</strong> He lives in a large cyclists community, so he wants to see other routes where other cyclists have rode on or discovered.</ul>
 
 			<h2>Use Case</h2>
-				<p>Steve Morrissey is about to leave home on his 10 speed Trek. He logs into his Strava account and reaches his profile. He presses the "Record". The app shows a map with the location of where he is currently at. Morrissey now presses the "Start" button ad now he can begin his bike ride. while he is riding his bike, he decides to take a short break at a local coffee shop. the phone recognizes that he is not moving in his regular pace and now stops recording his progress. after 12 min, before he begins his bike ride, looks at his phone and taps the "Continue" button. the phone now begins to record his progress by GPS. He reaches his destination and on his phone taps the "Finish" button. a screen comes up and shows the path he rode, how many miles he did, the time it took from start to finish, and how many stops he made.  </p>
+				<p>Steve Morrissey is about to leave home on his 10 speed Trek. He logs into his Strava account and reaches his profile. He presses the "Record". The app shows a map with the location of where he is currently at. Morrissey now presses the "Start" button ad now he can begin his bike ride.</p>
+			<p>While he is riding his bike, he decides to take a short break at a local coffee shop. the phone recognizes that he is not moving in his regular pace and now stops recording his progress. after 12 min, before he begins his bike ride, looks at his phone and taps the "Continue" button. the phone now begins to record his progress by GPS.</p>
+			<p>He reaches his destination and on his phone taps the "Finish" button. a screen comes up and shows the path he rode, how many miles he did, the time it took from start to finish, and how many stops he made.</p>
+
 			<h2>Interaction Flow</h2>
-				<p></p>
-			<h2>Conceptual Model</h2>
-				<p></p>
+				<ul><strong>1.</strong> Open app</ul>
+				<ul><strong>2.</strong> At main menu, taps "Start".</ul>
+				<ul><strong>3.</strong> live feed map screen comes up.</ul>
+				<ul><strong>4.</strong> Taps "Start" and begins to ride</ul>
+				<ul><strong>5.</strong> Taps "Continue" button after break.</ul>
+				<ul><strong>6.</strong> Taps "Finish" button after reaching destination.</ul>
+				<ul><strong>7.</strong> Looks at progress on screen than presses "Save"</ul>
+				<ul><strong>8.</strong> Data saves into app along with map trail.</ul>
 		</main>
 	</body>
 </html>
