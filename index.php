6c33eed086bc2a4ce74ec39a66e2a0be861f094b
Joseph Ramirez <jramirez98@cnm.edu>
2017-01-12 09:47:33 -0700

    inserted bullet points

--- index.php
+++ index.php
@@ -12,12 +12,14 @@
 		<main>
 			<!-- Name, Age, Occupation,Technology, Needs, Goals-->
 			<h2>Persona</h2>
-			<ul><strong>Name:</strong> Morrissey, Steve</ul>
-			<ul><strong>Age:</strong> 27 years-old</ul>
-				<ul><strong>Occupation:</strong> Works in Bloomingdale's men's fashionable department in Palo Alto, Calif.</ul>
-				<ul><strong>Technology:</strong> Uses Iphone 7.</ul>
-				<ul><strong>Needs:</strong> Steve biggest hobby is competing in single-day and time-trial bicycle races. So he needs to keep track of distance and routes he rides. And, he is also appearance and health conscious, so working in in Palo Alto, he is able to to ride his bicycle to and from work.</ul>
-				<ul><strong>Goals:</strong> He lives in a large cyclists community, so he wants to see other routes where other cyclists have rode on or discovered.</ul>
+				<ul>
+				<li><strong>Name:</strong> Morrissey, Steve</li>
+				<li><strong>Age:</strong> 27 years-old</li>
+				<li><strong>Occupation:</strong> Works in Bloomingdale's men's fashionable department in Palo Alto, Calif.</li>
+				<li><strong>Technology:</strong> Uses Iphone 7.</li>
+				<li><strong>Needs:</strong> Steve biggest hobby is competing in single-day and time-trial bicycle races. So he needs to keep track of distance and routes he rides. And, he is also appearance and health conscious, so working in in Palo Alto, he is able to to ride his bicycle to and from work.</li>
+				<li><strong>Goals:</strong> He lives in a large cyclists community, so he wants to see other routes where other cyclists have rode on or discovered.</li>
+				</ul>
 
 			<h2>Use Case</h2>
 				<p>Steve Morrissey is about to leave home on his 10 speed Trek. He logs into his Strava account and reaches his profile. He presses the "Record". The app shows a map with the location of where he is currently at. Morrissey now presses the "Start" button ad now he can begin his bike ride.</p>
